99493f23d75d99e75e44c3eaf5f6ca6d211ec16e
jmonterosantiago <j.montero.santiago@accenture.com>
2025-12-01 17:43:36 +0000

    fix: api_root uses DRF decorator and Response; serializers handle ObjectId; settings ALLOWED_HOSTS for codespace

--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,27 +1,50 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+
 class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    def get_id(self, obj):
+        return str(obj.id)
     class Meta:
         model = Team
         fields = '__all__'
 
+
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    team = TeamSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
     class Meta:
         model = User
         fields = '__all__'
 
+
 class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    user = UserSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
     class Meta:
         model = Activity
         fields = '__all__'
 
+
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    def get_id(self, obj):
+        return str(obj.id)
     class Meta:
         model = Workout
         fields = '__all__'
 
+
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+    team = TeamSerializer(read_only=True)
+    def get_id(self, obj):
+        return str(obj.id)
     class Meta:
         model = Leaderboard
         fields = '__all__'
